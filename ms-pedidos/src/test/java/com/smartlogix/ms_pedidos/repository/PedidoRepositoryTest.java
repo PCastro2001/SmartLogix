795c8723021fcaab6f52ef518941ce9e43c361d7
PCastro2001 <chicastro2001@gmail.com>
2026-05-12 17:27:15 -0400

    fix(ms-pedidos): actualizar package de @DataJpaTest para Spring Boot 4.0 y restaurar dependencias de testing

--- ms-pedidos/src/test/java/com/smartlogix/ms_pedidos/repository/PedidoRepositoryTest.java
+++ ms-pedidos/src/test/java/com/smartlogix/ms_pedidos/repository/PedidoRepositoryTest.java
@@ -6,7 +6,7 @@ import com.smartlogix.ms_pedidos.model.Pedido;
 import jakarta.validation.ConstraintViolationException;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.data.jpa.test.autoconfigure.DataJpaTest;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
